9637981d016e52bccf7b55d3225f072aaaf3ac77
ShMcK <shawn.j.mckay@gmail.com>
2016-11-23 19:40:53 -0800

    setup password pre-commit hash

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,11 +1,49 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  email: String,
-  password: String,
+  username: {
+    type: String,
+    lowercase: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
   admin: Boolean,
 })
+
+// hash passwords before saving them to the db
+userSchema.pre('save', function (next) {
+  const user = this
+  if (user.isModified('password') || user.isNew) {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) return next(err)
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err)
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    return next()
+  }
+})
+
+// compare passwords in db using hashing algorithm
+userSchema.methods.comparePassword = function (pw, cb) {
+  bcrypt.compare(pw, this.password, (err, isMatch) => {
+    if (err) return cb(err)
+    cb(null, isMatch)
+  })
+}
 
 const User = mongoose.model('User', userSchema)
 
