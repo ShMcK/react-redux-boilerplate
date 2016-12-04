679adc9217a34afdadf40a09e6f1471e73b2c80e
ShMcK <shawn.j.mckay@gmail.com>
2016-12-03 21:14:25 -0800

    add antd

--- src/containers/render.js
+++ src/containers/render.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import Router from './router'
 
+import 'antd/dist/antd.css'
+
 const render = (store) => {
     ReactDOM.render(
         <Provider store={store}>
