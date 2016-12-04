38abe55a5fc9a74a75084a854b8cc03383e0c31c
ShMcK <shawn.j.mckay@gmail.com>
2016-12-03 21:14:30 -0800

    add navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import './navbar.css'
 
-const NavBar = (props) => (
-  <div>NavBar</div>
+const NavBar = () => (
+  <nav className='navbar'>
+    <div className='navbar-left'>
+      <span className='brand'>Brand</span>
+    </div>
+    <div className='navbar-right'>
+      <span className='login'>Login</span>
+    </div>
+  </nav>
 )
 
 export default NavBar
